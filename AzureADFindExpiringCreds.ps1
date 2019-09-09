39a05db346e38d488ba096aa435ea48d4b678487
mattbowes <30603462+mattbowes@users.noreply.github.com>
2019-09-09 10:08:03 -0700

    Create AzureADFindExpiringCreds.ps1

---
+++ AzureADFindExpiringCreds.ps1
@@ -0,0 +1,43 @@
+$daystomonitor = 90
+$results = @()
+
+$azureapps = Get-AzureRMADApplication 
+foreach ($app in $azureapps) {
+
+    $expiringcred = Get-AzureRMADAppCredential -ObjectId $app.ObjectId
+    # an app can have more than one secret / cert
+    foreach ($e in $expiringcred) {
+
+        if ($null -ne $e.enddate) {
+            if ([datetime]$e.enddate -lt (get-date).AddDays($daystomonitor) -and ([datetime]$e.enddate -ge (get-date))) {
+            
+                $results += [PSCustomObject]@{
+                    AzureADName   = $app.displayname
+                    StartDate     = $e.StartDate
+                    EndDate       = $e.EndDate
+                    Type          = $e.Type
+                    applicationId = $app.applicationId
+                }
+            }
+        }
+    }
+}
+
+
+$azuresp = Get-AzureRMADServicePrincipal 
+foreach ($sp in $azuresp) {
+    $spc = get-AzureRMADServicePrincipalCredential -ObjectId $sp.id
+    foreach ($s in $spc) {
+        if ($null -ne $s.enddate) {
+            if ( [datetime]$s.enddate -lt (get-date).AddDays($daystomonitor) -and ([datetime]$s.enddate -ge (get-date))) {
+                $results += [PSCustomObject]@{
+                    AzureADName   = $sp.DisplayName
+                    StartDate     = $s.StartDate
+                    EndDate       = $s.EndDate
+                    Type          = $s.Type
+                    applicationId = $sp.applicationId
+                }
+            }
+        }
+    }
+}
