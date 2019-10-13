32a437a6f10907092135d8319e850ae5175e2cd0
mattbowes <30603462+mattbowes@users.noreply.github.com>
2019-10-13 14:16:30 -0700

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,5 +1,6 @@
+# for use with ConEmu 
 ## Dracula readline configuration. Requires version 2.0, if you have 1.2 convert to `Set-PSReadlineOption -TokenType`
-
+<#
 Set-PSReadlineOption -Color @{
     "Command"   = [ConsoleColor]::Green
     "Parameter" = [ConsoleColor]::Gray
@@ -11,17 +12,19 @@ Set-PSReadlineOption -Color @{
     "Comment"   = [ConsoleColor]::DarkCyan
 }
 
+#>
 
 
 # Dracula Prompt Configuration
 Import-Module posh-git
-#$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
+$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
 $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
 $GitPromptSettings.DefaultPromptPath.ForegroundColor = [ConsoleColor]::Cyan
-$GitPromptSettings.DefaultPromptSuffix.Text = '> ' # chevron unicode symbol
+#$GitPromptSettings.DefaultPromptSuffix.Text = '> ' # chevron unicode symbol
 $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
 # Dracula Git Status Configuration
 $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $false
-$GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::DarkCyan
-$GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::DarkCyan      
-$GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::DarkCyan
+#$GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::DarkCyan
+#$GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::DarkCyan      
+#$GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::DarkCyan
+$GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
